733c980c933c296a8fec1d9987bd6b1f78fba992
Stephen Quan <squan@esri.com>
2026-01-15 14:19:39 +1100

    Added support for MacOS / iOS

--- MauiDevLab/InputViewHelper.MaciOS.cs
+++ MauiDevLab/InputViewHelper.MaciOS.cs
@@ -6,5 +6,17 @@ public static partial class InputViewHelper
 {
 	public static partial void SetBorderless(this InputView inputView)
 	{
+		if (inputView.Handler?.PlatformView is UIKit.UITextField textField)
+		{
+			textField.BackgroundColor = UIKit.UIColor.Clear;
+			textField.Layer.BorderWidth = 0;
+			textField.BorderStyle = UIKit.UITextBorderStyle.None;
+		}
+
+		if (inputView.Handler?.PlatformView is UIKit.UITextView textView)
+		{
+			textView.BackgroundColor = UIKit.UIColor.Clear;
+			textView.Layer.BorderWidth = 0;
+		}
 	}
 }
--- MauiDevLab/MainPage.xaml.cs
+++ MauiDevLab/MainPage.xaml.cs
@@ -9,15 +9,9 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	void Editor_Loaded(object sender, EventArgs e)
+	void InputView_Loaded(object sender, EventArgs e)
 	{
 		InputView inputView = (InputView)sender;
 		inputView.SetBorderless();
-	}
-
-	void Editor_Focused(object sender, FocusEventArgs e)
-	{
-		InputView inputView = (InputView)sender;
-		inputView.Dispatcher.Dispatch(() => inputView.SetBorderless());
 	}
 }
